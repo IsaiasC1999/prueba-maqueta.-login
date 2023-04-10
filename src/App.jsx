ff490688f59eb5e9c630954987d6ff8147f9ce9f
IsaiasC1999 <isaias.romano99@gmail.com>
2023-04-09 22:58:53 -0300

    update de estilos

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Header from './components/Header/Header'
 import Contenido from './components/Main/Contenido'
 import Aside from './components/Aside/Aside'
 import Footer from './components/Footer/Footer'
+import Menu from './components/Menu/Menu'
 
 function App() {
-  
+
 
   return (
-    <div className="Layout-app">
-        <Header/>
+    <>
+    <Header/>
+    <div className="content-main">
+        <Menu/>  
         <Contenido/>
         <Aside/>
         <Footer/>        
     </div>
+    </>
   )
 }
 
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Menu.css"
+function Menu() {
+    return ( 
+        <div className="menu-principal">
+            menu-princial
+        </div>
+     );
+}
+
+export default Menu;
